ac251e9aad63931f3979efb7b07a3e4922bf738f
dajana-omnion <dajana@webrookie.xyz>
2018-03-21 11:16:38 +0100

    scalingo db conn

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '163.172.75.151'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'dajanane_hm'),
-            'username' => env('DB_USERNAME', 'dajanane_hm'),
-            'password' => env('DB_PASSWORD', 'anajad.123'),
+            'host' => env('DB_HOST', '0ff55ad9-76ae-4781-bd10-a7e7a88d72d7.omni-hotel-1162.mysql.dbs.scalingo.com'),
+            'port' => env('DB_PORT', '31447'),
+            'database' => env('DB_DATABASE', 'omni-hotel-1162 '),
+            'username' => env('DB_USERNAME', 'omni_hotel_1162'),
+            'password' => env('DB_PASSWORD', 'odBrd1qTfa7yiYJl9QZ9@0ff55ad9-76ae-4781-bd10-a7e7a88d72d7'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',

6f23fc4ab6af0244736e49446820c486150f90d3
dajana-omnion <dajana@webrookie.xyz>
2018-03-21 11:03:19 +0100

    remote db

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '163.172.75.151'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'dajanane_hm'),
+            'username' => env('DB_USERNAME', 'dajanane_hm'),
+            'password' => env('DB_PASSWORD', 'anajad.123'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
